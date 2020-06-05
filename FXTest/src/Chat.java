3e7bc03d047d892d5fb90b97f8b616929e4e625c
MeritonKyqyku <43965461+MeritonKyqyku@users.noreply.github.com>
2020-06-05 02:27:49 +0200

    Pjesa e serverit edhe klientave
    Klienti osht i marret prej interneti edhe me java.swing (awt) duhet me e kthy ne fx

--- FXTest/src/Chat.java
+++ FXTest/src/Chat.java
@@ -12,29 +12,49 @@ import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
-	public class Chat extends Application {
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import org.java_websocket.WebSocketImpl;
+import org.java_websocket.client.WebSocketClient;
+import org.java_websocket.drafts.Draft;
+import org.java_websocket.drafts.Draft_6455;
+import org.java_websocket.handshake.ServerHandshake;
+
+
+	public class Chat extends Application implements EventHandler<ActionEvent> {
+		 WebSocketClient cc;
+		 VBox vBox=new VBox();
+    	 HBox hBox=new HBox();
+    	Button btnsend= new Button("Send");
+    	Button callButton=new Button();
+    	ImageView img=new ImageView("Voice.png");
+    	
+    	Button videoButton=new Button();
+    	ImageView img1=new ImageView("Video.png");
+    	Button connect=new Button();
+    	
+    	Button close= new Button();
+    	
+    	TextArea Chat=new TextArea();
+    
+    	TextField message=new TextField();
 
 	    @Override
 	    public void start(Stage primaryStage) throws Exception{
-	    	VBox vBox=new VBox();
-	    	HBox hBox=new HBox();
-	    	Button btnsend= new Button("Send");
-	    	Button callButton=new Button();
-	    	ImageView img=new ImageView("Voice.png");
+	    	
 	    	img.setFitHeight(18);
+	    	connect.setOnAction(this);
+	    	
 	    	img.setFitWidth(18);
 	    	callButton.setGraphic(img);
-	    	Button videoButton=new Button();
-	    	ImageView img1=new ImageView("Video.png");
 	    	img1.setFitHeight(18);
 	    	img1.setFitWidth(20);
 	    	videoButton.setGraphic(img1);
-	    	TextArea Chat=new TextArea();
 	    	Chat.setPrefHeight(600);
 	    	Chat.setPrefWidth(300);
-	    	TextField message=new TextField();
 	    	message.setPrefWidth(180);
-	    	hBox.getChildren().addAll(message,btnsend,callButton,videoButton);
+	    	hBox.getChildren().addAll(message,btnsend,callButton,videoButton,connect,close);
 	    	vBox.getChildren().addAll(Chat,hBox);
 	    	StringBuilder fieldContent = new StringBuilder(""); 
 	    	btnsend.setOnAction(new EventHandler<ActionEvent>() {
@@ -51,12 +71,72 @@ import javafx.stage.Stage;
 	        primaryStage.setScene(scene);
 	        primaryStage.setResizable(false);
 	        primaryStage.show();
+	        
+	        
+	        
+	        
 	    }
+	    public void actionPerformed( ActionEvent e ) {
+
+			if( e.getSource() == message) {
+				if( cc != null ) {
+					cc.send( message.getText() );
+					message.setText( "" );
+					message.requestFocus();
+				}
+
+			} else if( e.getSource() == connect ) {
+				try {
+					// cc = new ChatClient(new URI(uriField.getText()), area, ( Draft ) draft.getSelectedItem() );
+					cc = new WebSocketClient( new URI( "ws://localhost:81")) {
+
+						@Override
+						public void onMessage( String message ) {
+							Chat.appendText( "got: " + message + "\n" );
+							
+						}
+
+						@Override
+						public void onOpen( ServerHandshake handshake ) {
+							Chat.appendText( "You are connected to ChatServer: " + getURI() + "\n" );
+							
+						}
+
+						@Override
+						public void onClose( int code, String reason, boolean remote ) {
+							Chat.appendText( "You have been disconnected from: " + getURI() + "; Code: " + code + " " + reason + "\n" );
+							
+							
+						}
+
+						@Override
+						public void onError( Exception ex ) {
+							Chat.appendText( "Exception occured ...\n" + ex + "\n" );
+						
+							ex.printStackTrace();
+							
+						}
+					};
+
+					
+					cc.connect();
+				} catch ( URISyntaxException ex ) {
+					Chat.appendText("Sosht ka ban is not a valid WebSocket URI\n" );
+				}
+			} else if( e.getSource() == close ) {
+				cc.close();
+			}
+		}
 	    
 	    
 
 	    public static void main(String[] args) {
 	        launch(args);
 	    }
+		@Override
+		public void handle(ActionEvent event) {
+			// TODO Auto-generated method stub
+			
+		}
 	}
 
